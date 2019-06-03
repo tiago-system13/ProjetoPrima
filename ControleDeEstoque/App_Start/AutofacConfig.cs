8b2737c188cee1b0781135fe05a3d7db97788255
tiago-system13 <tiago.infor13@gmail.com>
2019-06-02 22:26:06 -0300

    removendo bin e obj versionado

--- ControleDeEstoque/App_Start/AutofacConfig.cs
+++ ControleDeEstoque/App_Start/AutofacConfig.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Autofac;
+using Autofac;
 using Autofac.Integration.Mvc;
 using aplicacao;
 using System;
@@ -34,41 +33,4 @@ namespace ControleDeEstoque.App_Start
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
-=======
-﻿using Autofac;
-using Autofac.Integration.Mvc;
-using aplicacao;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.Mvc;
-using dominio;
-
-namespace ControleDeEstoque.App_Start
-{
-    public class AutofacConfig
-    {
-        public static void Register()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterControllers(typeof(MvcApplication).Assembly);
-            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
-            builder.RegisterModelBinderProvider();
-            builder.RegisterModule<AutofacWebTypesModule>();
-            builder.RegisterSource(new ViewRegistrationSource());
-            builder.RegisterFilterProvider();
-            builder.Register(c => HttpContext.Current.User).InstancePerRequest();
-            builder.Register(c => HttpContext.Current.Session).InstancePerRequest();
-            /// <summary>
-            /// Registre aqui seu módulo
-            /// </summary>
-            builder.RegisterModule<DominioModulo>();
-            builder.RegisterModule<AplicacaoModulo>();
-            var container = builder.Build();
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-        }
-    }
->>>>>>> fad11f3e7c10c01b8efe32fd0d28703c9204a75f
 }
--- ControleDeEstoque/App_Start/FilterConfig.cs
+++ ControleDeEstoque/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Web;
+using System.Web;
 using System.Web.Mvc;
 
 namespace ControleDeEstoque
@@ -12,18 +11,3 @@ namespace ControleDeEstoque
         }
     }
 }
-=======
-﻿using System.Web;
-using System.Web.Mvc;
-
-namespace ControleDeEstoque
-{
-    public class FilterConfig
-    {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-    }
-}
->>>>>>> fad11f3e7c10c01b8efe32fd0d28703c9204a75f
